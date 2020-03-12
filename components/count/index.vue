5d32159071001e16aca6cdecbb3693f8039886ad
boom <327965509@qq.com>
2020-03-12 22:04:46 +0800

    完成部分计数组件

---
+++ components/count/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <span class="count">{{countNumber}}</span>
+</template>
+
+<script>
+export default {
+    props: {
+        number: {
+            type: [String, Number]
+        },
+        duration: {
+            type: [String, Number],
+            default: 1000
+        }
+    },
+    data() {
+        return {
+            countNumber: 0,
+            timeId: null
+        }
+    },
+    mounted() {
+        let step = 1
+        // const count = this.number / this.step
+        // const interval =
+
+        this.timeId = setInterval(() => {
+            if (this.countNumber + step >= this.number) {
+                this.countNumber = this.number
+                clearInterval(this.timeId)
+            } else {
+                this.countNumber += step
+            }
+        }, 10)
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.count {
+}
+</style>
---
+++ test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="test">
+        <count :number="1000"></count>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        Count: () => import('./components/count')
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.test {
+}
+</style>
